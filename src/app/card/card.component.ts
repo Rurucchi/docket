f795be31553560be9f924f5a4dc36a89d6ede729
Rurucchi <rurucchi.pro@gmail.com>
2023-05-21 17:09:23 +0200

    added layout and style, todo : component appending on button press

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Injectable, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css'],
 })
+@Injectable()
 export class CardComponent {
-  @Input() name: string | undefined;
+  @Input() bgColor: string | undefined;
 }
